87e48e20c541b5ce3001c18fda14fbf8356d6abc
zhangxi <zhangx368@mail2.sysu.edu.cn>
2018-11-30 21:07:17 +0800

    add instruction, fix script_path

--- extract_features/extract_features.sh
+++ extract_features/extract_features.sh
@@ -3,17 +3,15 @@
 rm *_hash.txt
 rm *_label.txt
 
-TOOLS=/home/xizhang/caffe/build/tools \
+TOOLS=../caffe/build/tools \
 
 QUERY=image
 RETRIEVAL=bows
 GPU=3
 DATA=iapr
-MODEL=/data2/zhangxi/models/iapr-tc12/unsupervised_16_cross_0.002_iter_9600.caffemodel
+MODEL=/data2/zhangxi/models/iapr-tc12/your_model_name.caffemodel
 $TOOLS/extract_features_txt.bin $MODEL ${DATA}_extract_features_retrieval.prototxt label,${RETRIEVAL}/classifier_hash retrieval_label.txt,retrieval_hash.txt 18000 GPU ${GPU}
 $TOOLS/extract_features_txt.bin $MODEL ${DATA}_extract_features_test.prototxt label,${QUERY}/classifier_hash test_label.txt,test_hash.txt 2000 GPU ${GPU}
-
-#matlab -nodesktop -nosplash -nojvm -r "run ./run_mir.m;quit;"
 
 #mir-25k:
 #retrieval:18049
@@ -21,9 +19,9 @@ $TOOLS/extract_features_txt.bin $MODEL ${DATA}_extract_features_test.prototxt la
 #test:1966
 
 #nus-wide:
-#retrieval:195834 = 3060*64
+#retrieval:195834
 #train:10500
-#test:2100 = 35*60
+#test:2100
 
 #iapr-tc12:
 #retrieval:18000
